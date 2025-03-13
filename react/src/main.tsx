90ad7f2613dd4f571593c167bf04c8dcffdd1821
rccalpito <rccalpito@gmail.com>
2025-03-13 10:08:59 -0700

    pingit from the frontend

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL || 'http://127.0.0.1:3000';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
