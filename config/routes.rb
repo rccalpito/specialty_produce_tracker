1c56fe02ea12178adf608050b877fbc440a30af0
rccalpito <rccalpito@gmail.com>
2025-03-18 14:54:11 -0700

    add endpoint and action for webhooks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :items
   resources :reciepts
-  resources :twilio, only: [:new, :create, :show, :index]
+
+  post "/webhooks/twilio", to: "webhooks#twilio"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
