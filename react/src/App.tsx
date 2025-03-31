63f643fd33d0e46d579d2965b1a2d59c53163beb
rccalpito <rccalpito@gmail.com>
2025-03-31 13:51:43 -0700

    initial implementation for line chart

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,34 +1,70 @@
 import { useState, useEffect } from 'react';
-import './App.css'
-import axios from 'axios'
+import './App.css';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 type Item = {
+  name: string;
   price: number;
   qty: number;
-}
+};
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Item Prices',
+    },
+  },
+};
 
 function App() {
-  const [items, setItems] = useState<Item[]>([])
+  const [items, setItems] = useState<Item[]>([]);
 
   useEffect(() => {
-    axios.get('/items').then(response => setItems(response.data)).catch(console.error)
-  }, [])
-  console.log(items)
+    axios
+      .get('/items')
+      .then((response) => setItems(response.data))
+      .catch(console.error);
+  }, []);
+
+  const labels = items.map((item) => item.name);
+  const prices = items.map((item) => item.price);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Price',
+        data: prices,
+        borderColor: 'rgb(75, 192, 192)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      },
+    ],
+  };
 
   return (
-
-    <div>{items.length > 0 ? (
-      items.map((item, index) => (
-        <ul>
-          <li key={index}>
-            <strong>Price: </strong> ${item.price} <br/>
-            <strong>Qty: </strong> ${item.qty} <br/>
-          </li>
-        </ul>
-      ))
-    ) : (<div>no items</div>)}
+    <div>
+      <h1>Items Line Chart</h1>
+      <Line options={options} data={data} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
