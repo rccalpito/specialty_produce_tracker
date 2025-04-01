fa9e50b7c50d68e8559199faf1b85fb693378a9d
rccalpito <rccalpito@gmail.com>
2025-04-01 15:49:08 -0700

    update schema to have item qty be decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_20_004055) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_01_222504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.decimal "price"
-    t.integer "qty"
+    t.decimal "qty", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
