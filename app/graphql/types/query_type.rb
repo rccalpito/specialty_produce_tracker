f2ae9d4fb897b24c31ef4938d9f2d0050bd6ce60
rccalpito <rccalpito@gmail.com>
2025-05-10 23:17:41 -0700

    add priceHistory query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,6 +7,11 @@ module Types
       argument :receipt_id, ID, required: true
     end
 
+    field :price_history, [ Types::ItemType ], null: true, description: "Fetches price history on item" do
+      argument :name, String, required: true
+      argument :receipt_id, ID, required: false
+    end
+
     field :receipt, Types::ReceiptType, null: false do
       argument :id, ID, required: true
     end
@@ -21,6 +26,12 @@ module Types
 
     def items(receipt_id:)
       Item.where(receipt_id: receipt_id)
+    end
+
+    def price_history(name:, receipt_id: nil)
+      scope = Item.where(name: name)
+      scope = scope.where(receipt_id: receipt_id) if receipt_id.present?
+      scope
     end
   end
 end
